ee73ddc66a8d17b789215ccc41c901fdfd34d62c
Reddyop <166029960+Reddyop@users.noreply.github.com>
2026-01-13 13:39:58 +0530

    Delete server directory

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handleGenerate } from '../controllers/generateController.js';
-
-const router = express.Router();
-
-// POST /api/generate
-router.post('/generate', handleGenerate);
-
-export default router;
